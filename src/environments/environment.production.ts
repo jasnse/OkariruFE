628ff429458f5b9c841e1a08632f21fb7d5387b3
Jason Theofillus Gunawan <jasonoke03@gmail.com>
2026-09-22 00:19:59 +0700

    fix placeholder

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // TODO: ganti ke URL backend production kalau sudah deploy
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1'
+  apiUrl: 'http://35.184.39.133:8080/api/v1'
 };
